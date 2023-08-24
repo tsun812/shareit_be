2cce21472f930033432b1a8d96aea4ba48b0cd25
tsun812 <tsun812@gmail.com>
2023-08-23 19:41:28 -0700

    set up rooms

--- index.ts
+++ index.ts
@@ -3,31 +3,30 @@ const http = require('http');
 const express = require('express');
 const app = express()
 const server = http.createServer(app);
-server.listen(3001)
+
 interface ServerToClientEvents {
-    'receive-changes': (delta: any) => void
-  }
-  
-interface ClientToServerEvents {
-    'send-changes': (delta: any) => void
+  'receive-changes': (delta: any) => void,
+  'load-document': (delta: any) => void
 }
 
-interface InterServerEvents {
-    ping: () => void;
-  }
-  
-  interface SocketData {
-    name: string;
-    age: number;
-  }
-  
+interface ClientToServerEvents {
+  'send-changes': any,
+  'get-document': any
+}
 const io = new Server(server, {cors: {
   origin: 'http://localhost:3000',
   method: ['GET', 'POST']
 }})
 
 io.on('connection', (socket: any) => {
-    socket.on('send-changes', (delta: any) => {
-        socket.broadcast.emit('receive-changes', delta)
+    socket.on('get-document', async(documentId: string) => {
+      socket.join(documentId)
+      io.to(documentId).emit('load-document', 'asdf')
+
+      socket.on('send-changes', (delta: any) => {
+          socket.to(documentId).emit('receive-changes', delta)
+      })
     })
 })
+
+server.listen(3001)
