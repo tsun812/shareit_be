b99d4deaf29c85d85c5efaf2334b0d854ec5608c
tsun812 <tsun812@gmail.com>
2023-08-24 18:08:59 -0700

    add model, create/update document

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
-const { Server } = require('socket.io')
-const http = require('http');
-const express = require('express');
+import { Server } from 'socket.io';
+import http from 'http';
+import express from 'express';
+import mongoose from 'mongoose';
+import DocumentSchema from './DocumentSchema';
+//import DocumentSchema  from "./DocumentSchema";
 const app = express()
 const server = http.createServer(app);
-
+mongoose.connect('mongodb://localhost:27017/shareit');
+mongoose.set('debug', true);
 interface ServerToClientEvents {
   'receive-changes': (delta: any) => void,
   'load-document': (delta: any) => void
@@ -14,19 +18,36 @@ interface ClientToServerEvents {
   'get-document': any
 }
 const io = new Server(server, {cors: {
-  origin: 'http://localhost:3000',
-  method: ['GET', 'POST']
+  origin: 'http://localhost:3000'
 }})
 
 io.on('connection', (socket: any) => {
-    socket.on('get-document', async(documentId: string) => {
+    socket.on('get-document', async (documentId: string) => {
+      const document = await findOrCreateDocument(documentId)
       socket.join(documentId)
-      io.to(documentId).emit('load-document', 'asdf')
-
+      if(document){
+        io.to(documentId).emit('load-document', document.data)
+      }
       socket.on('send-changes', (delta: any) => {
-          socket.to(documentId).emit('receive-changes', delta)
+        socket.to(documentId).emit('receive-changes', delta)
       })
+
+      socket.on('save-changes', async (delta: any) => {
+        await DocumentSchema.findByIdAndUpdate(documentId, {data: delta})
+    })
+
     })
 })
+
+const findOrCreateDocument = async (id: string) => {
+  if(id == null) return
+  const document = await DocumentSchema.findById(id)
+  if(document){
+    return document
+  }
+  else {
+    return await DocumentSchema.create({_id: id, data: ''})
+  }
+}
 
 server.listen(3001)
