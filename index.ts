821a4cdc0f6857a12d509bc1ea47c55e6f3af290
tsun812 <tsun812@gmail.com>
2023-09-03 16:30:01 -0700

    update index.ts

--- index.ts
+++ index.ts
@@ -9,35 +9,34 @@ const server: http.Server = http.createServer(app);
 mongoose.connect('mongodb://localhost:27017/shareit');
 mongoose.set('debug', true);
 interface ServerToClientEvents {
-  'receive-changes': (delta: any) => void,
-  'load-document': (delta: any) => void,
+  'receive-changes': (delta: object) => void,
+  'load-document': (delta: object) => void,
 }
 
 interface ClientToServerEvents {
-  'send-changes': (delta: any) => void,
+  'send-changes': (delta: object) => void,
   'get-document': (id: string) => void,
-  'save-changes': (delta: any) => void,
+  'save-changes': (delta: object) => void,
 }
 const io: Server<ClientToServerEvents, ServerToClientEvents>= new Server(server, {cors: {
   origin: 'http://localhost:3000'
 }})
 
 io.on('connection', (socket: Socket) => {
-    socket.on('get-document', async (documentId: string) => {
-      const document = await findOrCreateDocument(documentId)
-      socket.join(documentId)
-      if(document){
-        io.to(documentId).emit('load-document', document.data)
-      }
-      socket.on('send-changes', (delta: any) => {
-        socket.to(documentId).emit('receive-changes', delta)
-      })
-
-      socket.on('save-changes', async (delta: any) => {
-        await DocumentSchema.findByIdAndUpdate(documentId, {data: delta, lastAccessTime: Date.now()})
+  socket.on('get-document', async (documentId: string) => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    if(document){
+      io.to(documentId).emit('load-document', document.data)
+    }
+    socket.on('send-changes', (delta: object) => {
+      socket.to(documentId).emit('receive-changes', delta)
     })
 
+    socket.on('save-changes', async (document: object) => {
+      await DocumentSchema.findByIdAndUpdate(documentId, {data: document, lastAccessTime: Date.now()})
     })
+  })
 })
 
 const findOrCreateDocument = async (id: string) => {
